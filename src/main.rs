dfff09f3832fbbce99f29bd86dfad655cb752a81
Kyle Strand <kyle.strand@rms.com>
2019-06-30 16:07:42 -0600

    Use unstable 'unwind(allowed)' to unwind in Nightly

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+#![feature(unwind_attributes)]
+
 #[link(name = "DoWork", kind = "static")]
 extern {
     fn Entry(err_code: Option<extern fn (i32)>);
 }
 
 #[no_mangle]
+#[unwind(allowed)]
 pub extern "C" fn do_unwind(err_code: i32) {
     panic!(format!("Panic from 'DoUnwind'; error code: {}", err_code));
 }
